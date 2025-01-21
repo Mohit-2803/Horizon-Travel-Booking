d0fc634788fb01cf157febdbff073f6dd5c12933
Mohit-2803 <sharmamohit28082003@gmail.com>
2025-01-21 17:55:17 +0530

    removed-readme and new frotend_url

--- server/server.js
+++ server/server.js
@@ -56,7 +56,7 @@ app.use((req, res, next) => {
 // CORS configuration for handling actual requests
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL, // Allow only your frontend domain to make requests
+    origin: "https://horizon-blue-nu.vercel.app/", // Allow only your frontend domain to make requests
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: [
       "Content-Type",
